41ec0245a7596607b510325e8beccd5ea23b8f0c
NRevill <40209391+NRevill@users.noreply.github.com>
2018-06-13 12:57:49 +0200

    Add files via upload

---
+++ Test/Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			List<Dollars> myList = new List<Dollars>()
+			{
+				new Dollars { Amount = 1000 },
+				new Dollars { Amount = 2000 },
+				new Dollars { Amount = 3000 },
+				new Dollars { Amount = 10000 }
+			};
+
+			foreach (Dollars dollars in myList)
+			{
+				Console.WriteLine(dollars.Amount);
+			}
+			*/
+			/*
+			int oneThousand = 1000;
+			int twoThousand = 2000;
+			int threeThousand = 3000;
+			int tenThousand = 10000;
+
+			Console.WriteLine("In one year my salary is going to be " + oneThousand + " USD");
+			Console.WriteLine("In one year my salary is going to be " + twoThousand + " USD");
+			Console.WriteLine("In one year my salary is going to be " + threeThousand + " USD");
+			Console.WriteLine("In one year my salary is going to be " + tenThousand + " USD");
+
+			Console.ReadLine();
+			*/
+
+			for (int i = 5; i > -1; i--)
+			{
+				Console.WriteLine($"I can see {i} in this square");
+			}
+			Console.ReadLine();
+
+
+		}
+	}
+	/*
+	class Dollars
+	{
+		public int Amount { get; set; }
+	}
+	*/
+
+}
---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Console.WriteLine("Add a number");
+			var firstNumber = int.Parse(Console.ReadLine());
+			Console.WriteLine("Add a second number");
+			var secondNumber = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Add or multiply?");
+			var answer = Console.ReadLine();
+
+			if (answer == "add" || (answer == "Add"))
+			{
+				Console.WriteLine(firstNumber + secondNumber);
+			}
+			else
+			{
+				Console.WriteLine(firstNumber * secondNumber);
+			}
+			*/
+
+			/*
+			var firstNumber = int.Parse(Console.ReadLine());
+			var secondNumber = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(firstNumber + secondNumber);
+			Console.WriteLine(firstNumber - secondNumber);
+			Console.WriteLine(firstNumber * secondNumber);
+			
+
+			if (secondNumber == 0 || (firstNumber == 0))
+			{
+				Console.WriteLine("Can't divide by zero!");
+			}
+			else
+			{
+				Console.WriteLine(firstNumber / secondNumber);
+			}
+			*/
+
+			/*
+			int count = int.Parse(Console.ReadLine());
+			int sum = 0;
+
+			for (int i = 0; i < count; i++)
+			{
+				Console.WriteLine($"Input size of file {i + 1}:");
+				sum = sum + int.Parse(Console.ReadLine());
+			}
+			Console.WriteLine($"Total disk space is {sum} bytes.");
+
+	        */
+
+			/*
+			int count = int.Parse(Console.ReadLine());
+			int min = int.MaxValue;
+
+			for (int i = 0; i < count; i++)
+			{
+				int newNumber = int.Parse(Console.ReadLine());
+
+				if (min > newNumber)
+					min = newNumber;
+			}
+
+			Console.WriteLine($"{min}");
+			*/
+
+			/*
+			Console.WriteLine("Write a number: ");
+			int min = int.Parse(Console.ReadLine());
+			Console.WriteLine("Write a second number: ");
+			int max = int.Parse(Console.ReadLine());
+
+			int sum = 0;
+
+			if (min > max)
+			{
+				Console.WriteLine("min should be smaller than max!");
+			}
+			else
+			{
+				for (int i = min; i < max; i++)
+				{
+					sum += i * i;
+				}
+
+				Console.WriteLine(sum);
+			}
+			*/
+
+			/*
+			int charCode = Console.Read();
+			char theRealChar = Convert.ToChar(charCode);
+
+			if (char.IsDigit(theRealChar))
+			{
+				Console.WriteLine("Digit");
+			}
+			if (char.IsLetter(theRealChar))
+			{
+				Console.WriteLine("Letter");
+			}
+			else
+			{
+				Console.WriteLine("Not a digit and not a letter");
+			}
+			*/
+			/*
+			// This is the same as
+			int count = int.Parse(Console.ReadLine());
+
+			int rows = 0;
+			while (rows++ < count)
+			{
+				int symbolsInRow = rows;
+
+				while (symbolsInRow-- > 0)
+					Console.Write('#');
+
+				Console.WriteLine();
+			}
+
+			// This
+			int count = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < count; ++i)
+			{
+				for (int j = 0; j <= i; ++j)
+					Console.Write('#');
+
+				Console.WriteLine();
+			}
+			*/
+
+			
+
+			Console.ReadLine();
+			
+
+		}
+	}
+}
