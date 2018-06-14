3cd96bfa57cd6e2b9e9f3f949e3053889669ba58
NRevill <40209391+NRevill@users.noreply.github.com>
2018-06-14 14:31:31 +0200

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Methods
+{
+	class SimpleCalculator
+	{
+		static void Main(string[] args)
+		{
+			string type = Console.ReadLine();
+			int firstNumber = int.Parse(Console.ReadLine());
+			int secondNumber = int.Parse(Console.ReadLine());
+
+			if (type == "add")
+				Console.WriteLine(Add(firstNumber, secondNumber));
+			else if (type == "subtract")
+				Console.WriteLine(Add(firstNumber, secondNumber));
+			else if (type == "multiply")
+				Console.WriteLine(Add(firstNumber, secondNumber));
+			else if (type == "divide")
+				Console.WriteLine(Add(firstNumber, secondNumber));
+
+			Console.ReadLine();
+		}
+
+		static int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+		static int Subtract(int x, int y)
+		{
+			return x - y;
+		}
+
+		static int Multiply(int x, int y)
+		{
+			return x * y;
+		}
+
+		static int Divide(int x, int y)
+		{
+			return x / y;
+		}
+	}
+}
