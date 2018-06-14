968fb8f59bec81068a9af78b1e18a6e1f114386c
NRevill <40209391+NRevill@users.noreply.github.com>
2018-06-14 14:38:01 +0200

    Add files via upload

---
+++ FunctionCaller/FunctionCaller/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FunctionCaller
+{
+	class FunctionalModule
+	{
+		static void Main(string[] args)
+		{
+			string command = Console.ReadLine();
+
+			while (command != "exit")
+			{
+				char first = command[0];
+				char.ToLower(first);
+
+				if ((first >= 'a' && first <= 'f') || (first >= 'A' && first <= 'F'))
+				{
+					FromAToF(command);
+				}
+				else if ((first >= 'g' && first <= 'l') || (first >= 'G' && first <= 'L'))
+				{
+					FromGToL(command);
+				}
+				else if ((first >= 'm' && first <= 'r') || (first >= 'M' && first <= 'R'))
+				{
+					FromMToR(command);
+				}
+				else if ((first >= 's' && first <= 'z') || (first >= 'S' && first <= 'Z'))
+				{
+					FromSToZ(command);
+				}
+				command = Console.ReadLine();
+			}
+			
+
+		}
+
+		static void FromAToF(string command)
+		{
+			Console.WriteLine($"FromAToF executes {command}.");
+		}
+
+		static void FromGToL(string command)
+		{
+			Console.WriteLine($"FromGToL executes {command}.");
+		}
+
+		static void FromMToR(string command)
+		{
+			Console.WriteLine($"FromMToR executes {command}.");
+		}
+
+		static void FromSToZ(string command)
+		{
+			Console.WriteLine($"FromSToZ executes {command}.");
+		}
+	}
+}
